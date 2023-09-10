079d4591108290f6057e4a10e9197bfeafbcf7a6
Ayu Huang <star82620@gmail.com>
2023-09-10 19:38:00 +0800

    fix/ add coworking-space.html & adjust main.jsx

--- src/containers/CoworkingSpace.jsx
+++ src/containers/CoworkingSpace.jsx
@@ -1,15 +1,19 @@
+import Marquee from "../components/Marquee";
 import Banner from "../components/Banner";
 import AboutUs from "../components/AboutUs";
 import SpaceIntro from "../components/SpaceIntro";
 import SpacePricing from "../components/SpacePricing";
+import Footer from "../components/Footer";
 
 export default function CoworkingSpace() {
   return (
     <>
+      <Marquee />
       <Banner />
       <AboutUs />
       <SpaceIntro />
       <SpacePricing />
+      <Footer />
     </>
   );
 }
---
+++ src/main-coworking-space.jsx
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom/client";
+import CoworkingSpace from "./containers/CoworkingSpace";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<CoworkingSpace />);
